31c90341e0bc2ebf307310ae479d825dbaf247e1
Anand Graves <anand@voorhoede.nl>
2019-06-07 14:43:03 +0200

    feat: checkbox-input component and build script

---
+++ README.md
@@ -0,0 +1,20 @@
+# Checkbox input
+
+* A custom checkbox input which uses an inline SVG for the checked state.
+* The SVG scales along when the checkbox is resized.
+* The attibutes `role="presentation"` and `aria-hidden="true"` have been added to the SVG to prevent old screen readers from reading the SVG source code.
+
+## Demo
+
+View the [demo](https://voorhoede-checkbox-input.netlify.com/).
+
+## Keyboard support
+
+* Tab - Moves keyboard focus to the checkbox.
+* Space - Toggles checkbox between checked and unchecked states.
+
+## Accessibility
+
+Component has been tested on:
+
+* MacOS VoiceOver in Safari
---
+++ build.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+const demoFile = 'demo.html'
+const outputDir = 'dist'
+const componentRegex = /@@component@@/
+const stylesRegex = /@@styles@@/
+const componentName = componentArgument()
+
+init()
+
+function init(){
+	try {
+		const componentFile = fs.readFileSync(`${componentName}/${componentName}.html`, 'utf8')
+		const stylesFile = fs.readFileSync(`${componentName}/${componentName}.css`, 'utf8')
+		const demoContent = fs.readFileSync(demoFile, 'utf8')
+		const replaceHtml = demoContent.replace(componentRegex, componentFile)
+		const result = replaceHtml.replace(stylesRegex, stylesFile)
+
+		if (!fs.existsSync(outputDir)) {
+			fs.mkdirSync(outputDir)
+		}
+
+		fs.writeFileSync(`${outputDir}/${demoFile}`, result, 'utf8')
+	} catch (error) {
+		console.log(error)
+	}	
+}
+
+function componentArgument() {
+	const splitOnEqual = process.argv[2].split('=')
+	if (!(process.argv[2] && splitOnEqual.length === 2 && splitOnEqual[0] === 'component' && splitOnEqual[1])) {
+		console.log('Argument \'component\' is required. E.g.: node build.js component=checkbox-input')
+		return
+	}
+
+	return splitOnEqual[1]
+}
